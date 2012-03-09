a868040277f08c2ed1e5a5b88de38bfaf6c133d9
jllord <jessica@codeforamerica.org>
2012-03-08 23:40:16 -0800

    First lessons

---
+++ newline.js
@@ -0,0 +1,9 @@
+var string = "Gimme \nPizza"
+  console.log(string)
+
+console.log("Make it " + "finger lickin")
+
+console.log(typeof 555)
+
+console.log((4 >= 6 || "grass" != "green") &&
+   !(12 * 2 == 144 && true))
